7585f7d4bd82abe19e443c4c476d1ca987ee0709
Steven Follis <steven_follis@live.com>
2016-09-09 10:05:06 -0400

    Initial dotnet upload

---
+++ dotnet/src/DevCamp.WebApp/Controllers/NewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevCamp.WebApp.Controllers
+{
+    public class NewController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
