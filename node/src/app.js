0ade165d289998c91e9c1115159e90ce3497f71e
Steven Follis <steven_follis@live.com>
2016-09-08 20:15:47 -0400

    Adjusted startup

--- node/src/app.js
+++ node/src/app.js
@@ -69,4 +69,8 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+});
