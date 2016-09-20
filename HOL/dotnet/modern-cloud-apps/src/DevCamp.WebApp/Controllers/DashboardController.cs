c1df8151d1cb9abfc789d5799146a678b050819b
ivega@microsoft.com <ivega@microsoft.com>
2016-09-20 04:14:44 -0400

    Added API support

---
+++ HOL/dotnet/modern-cloud-apps/src/DevCamp.WebApp/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using DevCamp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevCamp.WebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+        public async Task<ActionResult> Index()
+        {
+            API.APIClient webApi = new API.APIClient();
+            API.IncidentOperations incIos = new API.IncidentOperations(webApi);
+            Task<List<Incident>> incidents = await incIos.GetAllIncidentsWithOperationResponseAsync();
+
+
+
+            return View(incidents);
+        }
+
+
+    }
+}
