57b6308a97ed4e64350f905e848f2766b5268cf8
Israel Vega <ivega@microsoft.com>
2016-09-30 08:56:36 -0400

    Adding about page

--- HOL/dotnet/modern-cloud-apps/end/DevCamp.WebApp/Controllers/HomeController.cs
+++ HOL/dotnet/modern-cloud-apps/end/DevCamp.WebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +12,24 @@ namespace DevCamp.WebApp.Controllers
         [AllowAnonymous]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Name = ClaimsPrincipal.Current.FindFirst("name").Value;
+
+            // The object ID claim will only be emitted for work or school accounts at this time.
+            Claim oid = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier");
+            ViewBag.ObjectId = oid == null ? string.Empty : oid.Value;
+
+            // The 'preferred_username' claim can be used for showing the user's primary way of identifying themselves
+            ViewBag.Username = ClaimsPrincipal.Current.FindFirst("preferred_username").Value;
+
+            // The subject or nameidentifier claim can be used to uniquely identify the user
+            ViewBag.Subject = ClaimsPrincipal.Current.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+
             return View();
         }
 
