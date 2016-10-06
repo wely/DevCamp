a560d3e44e2a7aa0736a3224d0cfebab2a28c9b2
Steven Follis <steven_follis@live.com>
2016-10-06 17:26:28 -0400

    Modified AI behavior

--- HOL/node/modern-cloud-apps/end/utilities/appInsights.js
+++ HOL/node/modern-cloud-apps/end/utilities/appInsights.js
@@ -9,12 +9,6 @@ module.exports.setup = function (app) {
         // Configure server side App Insights SDK
         appInsights.setup().start();
 
-        // Create route to return the AI Instrumentation Key
-        // Used by the client side libary to instantiate AI
-        app.get('/api/ai', function (req, res) {
-            res.json({ key: process.env.APPINSIGHTS_INSTRUMENTATIONKEY });
-        });
-
     }
 
 };
