224e57e9ef2a93734ebbf20dd03628eee57b2680
ivega@microsoft.com <ivega@microsoft.com>
2016-10-12 23:05:48 -0400

    Updating starter project for modern apps HOL

---
+++ HOL/dotnet/modern-cloud-apps/start/DevCamp.WebApp/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DevCamp.WebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            //##### API DATA HERE #####
+                //##### ADD CACHING HERE #####
+                //##### ADD CACHING HERE #####
+            //##### API DATA HERE #####
+
+            return View();
+        }
+    }
+}
