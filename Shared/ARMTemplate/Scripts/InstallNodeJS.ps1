62fa75b54066057de8157424527efa3c0a45992d
Steven Follis <steven_follis@live.com>
2016-11-22 10:52:08 -0500

    Added logging

--- Shared/ARMTemplate/Scripts/InstallNodeJS.ps1
+++ Shared/ARMTemplate/Scripts/InstallNodeJS.ps1
@@ -1,2 +1,15 @@
+# Ensure that current process can run scripts. 
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
+
 # Install NodeJS via Chocolatey
-choco install nodejs.install --x86 --force
+Write-Host "Installing package: NodeJS ..."
+
+# Install git via chocolatey.
+choco install nodejs.install --x86 --force --yes --acceptlicense --verbose --allow-empty-checksums | Out-Null  
+if (-not $?)
+{
+    Write-Host "Installation of NodeJS failed via Chocolatey."
+}
+else {
+    Write-Host "Successfully installed NodeJS via Chocolatey."
+}
