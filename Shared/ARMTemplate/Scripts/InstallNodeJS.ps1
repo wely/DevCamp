26a1955c83f5782212baedf6b49bdb4a72f6a53a
Steven Follis <steven_follis@live.com>
2016-11-22 12:38:57 -0500

    Added Choc install

--- Shared/ARMTemplate/Scripts/InstallNodeJS.ps1
+++ Shared/ARMTemplate/Scripts/InstallNodeJS.ps1
@@ -1,6 +1,9 @@
 # Ensure that current process can run scripts. 
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
 
+# Install Chocolatey
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
 # Install NodeJS via Chocolatey
 Write-Host "Installing package: NodeJS ..."
 
