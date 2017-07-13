ff82e76da3206957b15ee683175118a77f894221
IRRDC <jk@oneassist.de>
2017-07-13 12:56:02 +0200

    Added HOL 07 Bots

---
+++ HOL/java/07-bot/end/CityPowerBot/Controllers/MessagesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace CityPowerBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        public static Stream LastImage { get; set; } = null;
+        public static String LastImageType { get; set; } = String.Empty;
+        public static String LastImageName { get; set; } = String.Empty;
+
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                // Stores send images out of order.
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                var imageAttachment = activity.Attachments?.FirstOrDefault(a => a.ContentType.Contains("image"));
+                if (imageAttachment != null)
+                {
+                    LastImage = await GetImageStream(connector, imageAttachment);
+                    LastImageName = imageAttachment.Name;
+                    LastImageType = imageAttachment.ContentType;
+                    Activity reply = activity.CreateReply("Got your image!");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else
+                {
+                    // Creates a dialog stack for the new conversation, adds MainDialog to the stack, and forwards all messages to the dialog stack.
+                    await Conversation.SendAsync(activity, () => new MainDialog());
+                }
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+
+        private static async Task<Stream> GetImageStream(ConnectorClient connector, Attachment imageAttachment)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                // The Skype attachment URLs are secured by JwtToken,
+                // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+                // https://github.com/Microsoft/BotBuilder/issues/662
+                var uri = new Uri(imageAttachment.ContentUrl);
+                if (uri.Host.EndsWith("skype.com") && uri.Scheme == "https")
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetTokenAsync(connector));
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+                }
+
+                return await httpClient.GetStreamAsync(uri);
+            }
+        }
+
+        /// <summary>
+        /// Gets the JwT token of the bot. 
+        /// </summary>
+        /// <param name="connector"></param>
+        /// <returns>JwT token of the bot</returns>
+        private static async Task<string> GetTokenAsync(ConnectorClient connector)
+        {
+            var credentials = connector.Credentials as MicrosoftAppCredentials;
+            if (credentials != null)
+            {
+                return await credentials.GetTokenAsync();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HOL/java/07-bot/start/DataWriter/IncidentController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using IncidentAPI;
+using IncidentAPI.Models;
+using Microsoft.Rest;
+using System.IO;
+
+namespace DataWriter
+{
+    public class IncidentController
+    {
+        [HttpPost]
+        public static Boolean Create(String firstName, String lastName, String street, String city, String state, String zipCode, String phoneNumber, String description, String outageType, Boolean? isEmergency, Stream image, String imageName, String imageType)
+        {
+            Boolean added = false;
+            try
+            {
+                Incident newIncident = new Incident();
+                newIncident.FirstName = firstName;
+                newIncident.LastName = lastName;
+                newIncident.Street = street;
+                newIncident.City = city;
+                newIncident.State = state;
+                newIncident.ZipCode = zipCode;
+                newIncident.PhoneNumber = phoneNumber;
+                newIncident.Description = description;
+                newIncident.OutageType = outageType;
+                newIncident.IsEmergency = isEmergency;
+
+                using (IncidentAPIClient client = GetIncidentAPIClient())
+                {
+                    var result = client.IncidentOperations.CreateIncident(newIncident);
+                    Newtonsoft.Json.Linq.JObject jobj = (Newtonsoft.Json.Linq.JObject)result;
+                    newIncident = jobj.ToObject<Incident>();
+                    added = true;
+                }
+
+                if (image != null)
+                {
+                    //Give the image a unique name based on the incident id
+                    var imageUrl = StorageHelper.UploadFileToBlobStorage(newIncident.Id, image, imageType, imageName);
+
+                    //Add a message to the queue to process this image
+                    StorageHelper.AddMessageToQueue(newIncident.Id, imageName);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            return added;
+        }
+
+        public static IncidentAPIClient GetIncidentAPIClient()
+        {
+            ServiceClientCredentials creds = new BasicAuthenticationCredentials();
+            var client = new IncidentAPIClient(new Uri("YOUR INCIDENT API URL"), creds);
+            return client;
+        }
+    }
+}
