43142ffc873d77f3a925ff563e8b7253f938afca
David Weinstein (AzureCAT) <daweins@microsoft.com>
2018-02-27 11:43:50 -0700

    Moved Github-desktop out to powershell, as it's not behaving w/ the chocolatey artifact
    Yet another checksum issue

--- Shared/ARMTemplate/Scripts/extrachocolatey.ps1
+++ Shared/ARMTemplate/Scripts/extrachocolatey.ps1
@@ -6,15 +6,26 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 # Install Chocolatey
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
-# Install NodeJS via Chocolatey
+# Install microsoftazurestorageexplorer via Chocolatey
 Write-Host "Installing package: Microsoft Azure Storage Explorer..."
 
-# Install git via chocolatey.
 choco install microsoftazurestorageexplorer --x86 --force --yes --acceptlicense --verbose --allow-empty-checksums --ignore-checksums | Out-Null  
 if (-not $?)
 {
     Write-Host "Installation of Microsoft Azure Storage Explorer failed via Chocolatey."
 }
 else {
-    Write-Host "Successfully installed Microsoft Azure Storage Explorervia Chocolatey."
+    Write-Host "Successfully installed Microsoft Azure Storage Explorer via Chocolatey."
+}
+
+# Install Github via Chocolatey
+Write-Host "Installing package: Github..."
+
+choco install github --x86 --force --yes --acceptlicense --verbose --allow-empty-checksums --ignore-checksums | Out-Null  
+if (-not $?)
+{
+    Write-Host "Installation of Github failed via Chocolatey."
+}
+else {
+    Write-Host "Successfully installed Github via Chocolatey."
 }
